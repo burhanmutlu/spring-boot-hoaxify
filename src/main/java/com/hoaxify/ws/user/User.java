670f9f9a3fe6c2f6268d9d1f2c0e221ca9c3cdd8
DESKTOP-IAVPDO1\Burhan Mutlu <burhackmutlu@gmail.com>
2023-06-04 01:13:47 +0300

    User classı eklendi

---
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,24 @@
+package com.hoaxify.ws.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String username;
+    private String displayName;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", displayName='" + displayName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
