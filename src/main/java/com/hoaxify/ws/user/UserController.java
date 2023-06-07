e680b9c4d2c3e504119745075b75729ede325093
DESKTOP-IAVPDO1\Burhan Mutlu <burhackmutlu@gmail.com>
2023-06-07 17:29:25 +0300

    kayıt ol için validation eklendi

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,10 +1,16 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -12,10 +18,30 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    /* bu validation işlemleri reactjsde olsa sadece mobil uygulamaya geçtiğimizde
+     herşeyi baştan yapmak lazım. o yüzden backend kısımda hallediyoruz. */
+
     @PostMapping("/api/1.0/users")
-    public GenericResponse createUser(@RequestBody Users user) {
+    public ResponseEntity<?> createUser(@RequestBody Users user) {
+        ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
+        Map<String, String> validationErrors = new HashMap<>();
+
+        String username = user.getUsername();
+        String displayName = user.getDisplayName();
+
+        if (username == null || username.isEmpty()) {
+            validationErrors.put("username", "Username cannot be null");
+        }
+        if (displayName == null || displayName.isEmpty()) {
+            validationErrors.put("displayName", "Cannot be null");
+        }
+        if(validationErrors.size() > 0) {
+            error.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+
         userService.save(user);
-        return new GenericResponse("user created");
+        return ResponseEntity.ok( new GenericResponse("user created") );
     }
 
 }
